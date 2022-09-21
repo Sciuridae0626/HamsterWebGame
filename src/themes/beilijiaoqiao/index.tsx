70ce658eb38d6c92a304fc6e65bf2eb9a3356555
Sciuridae0626 <sciuridae0626@gmail.com>
2022-09-21 19:48:29 +0800

    2022.9.21

--- src/themes/beilijiaoqiao/index.tsx
+++ src/themes/beilijiaoqiao/index.tsx
@@ -1,23 +1,33 @@
-// 钓鱼佬主题
+// 北立交桥主题
 import React from 'react';
 import { Theme } from '../interface';
 import { DefaultSoundNames, defaultSounds } from '../default';
+
+// const soundUrls = import.meta.glob('./sounds/*.mp3', {
+//     import: 'default',
+//     eager: true,
+// });
+
+// const sounds = Object.entries(soundUrls).map(([key, value]) => ({
+//     name: key.slice(9, -4),
+//     src: value,
+// })) as Theme<string>['sounds'];
 
 const imagesUrls = import.meta.glob('./images/*.png', {
     import: 'default',
     eager: true,
 });
 
-const fishes = Object.entries(imagesUrls).map(([key, value]) => ({
+const images = Object.entries(imagesUrls).map(([key, value]) => ({
     name: key.slice(9, -4),
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     content: <img src={value} alt="" />,
 }));
 
-export const fishermanTheme: Theme<DefaultSoundNames> = {
-    name: '钓鱼佬',
-    icons: fishes.map(({ name, content }) => ({
+export const beilijiaoqiaoTheme: Theme<DefaultSoundNames> = {
+    name: '北立交桥',
+    icons: images.map(({ name, content }) => ({
         name,
         content,
         clickSound: 'button-click',
